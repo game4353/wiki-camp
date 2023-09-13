f43ae19c4b2c1f58332763fe30f3e079db5ce539
game4353 <73753515+game4353@users.noreply.github.com>
2023-09-14 01:48:37 +0800

    fix nextJS missing some files... maybe

--- app/[lang]/api/[slug]/route.ts
+++ app/[lang]/api/[slug]/route.ts
@@ -2,16 +2,20 @@ import path from 'path'
 import { promises as fs } from 'fs'
 import { NextResponse } from 'next/server'
 
+// https://dev.to/martinratinaud/nextjs-on-vercel-missing-files-after-build-when-interpolating-file-names-40bl
+
 export async function GET (
   request: Request,
   { params: { lang, slug } }: { params: { lang: string, slug: string } }
 ) {
-  if (lang === 'zh') lang = 'zh-TW'
-  else if (lang === 'ja') lang = 'ja'
-  let jsonPath = path.join(process.cwd(), 'data', 'master', lang, `${slug}.json`)
+
+  let jsonPath = ''
+  if (lang === 'ja') jsonPath = path.join(process.cwd(), 'data/master/ja', `${slug}.json`)
+  if (lang === 'zh') jsonPath = path.join(process.cwd(), 'data/master/zh-TW', `${slug}.json`)
   if (lang === 'en') jsonPath = path.join(process.cwd(), 'data/master/en', `${slug}.json`)
+  if (lang === 'ko') jsonPath = path.join(process.cwd(), 'data/master/ko', `${slug}.json`)
+ 
   let jsonObj
-  
   try {
     // method 1
     const fileContents = await fs.readFile(jsonPath, 'utf8')
