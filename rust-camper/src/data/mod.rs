144b250e63ddf965da2edcb68adbd82e1a0146a1
game4353 <73753515+game4353@users.noreply.github.com>
2023-09-13 19:03:57 +0800

    fix the problem when app version changes

--- rust-camper/src/data/mod.rs
+++ rust-camper/src/data/mod.rs
@@ -11,7 +11,7 @@ use crate::{
         dir::{dir_master_json, path_json_asset},
         rw::write_json,
     },
-    game::{client::Client, cnst::Lan, version::get_cached_ver},
+    game::{client::Client, cnst::Lan, version::{get_cached_ver, app_version}},
     image::img_main,
 };
 use std::error::Error;
@@ -21,16 +21,19 @@ pub async fn data_main() -> Result<(), Box<dyn Error>> {
         // step 1: get cached version
         let old_vers = get_cached_ver(lan);
 
-        // step 2: obtain binary
-        let mut client = Client::account(1);
-        let (new_vers, master_bytes, resource_bytes) = client.login("", lan).await?;
+        // step 2: get latest version
+        let latest_ver = app_version().await?;
 
         // step 3: parse .dll to .cs
-        if old_vers.application != new_vers.application {
+        if old_vers.application != latest_ver {
             extract_cs()?;
         }
 
-        // step 4: read master binary & write sub master json
+        // step 4: obtain binary
+        let mut client = Client::account(1);
+        let (new_vers, master_bytes, resource_bytes) = client.login("", lan).await?;
+
+        // step 5: read master binary & write sub master json
         if old_vers != new_vers {
             let json = bin_to_json(master_bytes, "Pmaster::All");
             let folder = dir_master_json(&lan);
@@ -41,8 +44,8 @@ pub async fn data_main() -> Result<(), Box<dyn Error>> {
             })?;
         }
 
-        // step 5: read resource binary & write resource json
-        // step 6: download assets
+        // step 6: read resource binary & write resource json
+        // step 7: download assets
         if lan == Lan::JA && old_vers.resource != new_vers.resource {
             let json = bin_to_json(resource_bytes, "Pcommon::Resources");
             let path = path_json_asset(&lan);
@@ -50,7 +53,7 @@ pub async fn data_main() -> Result<(), Box<dyn Error>> {
             img_main().await;
         }
 
-        // step 7: run some git push
+        // step 8: run some git commit
         if old_vers != new_vers {
             auto_push(lan, &new_vers);
         }
