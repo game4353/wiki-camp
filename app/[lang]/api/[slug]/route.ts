af09615a8547a477ca79f2886b11c767175c169d
game4353 <73753515+game4353@users.noreply.github.com>
2023-09-14 01:34:41 +0800

    trying hinting languages

--- app/[lang]/api/[slug]/route.ts
+++ app/[lang]/api/[slug]/route.ts
@@ -7,15 +7,19 @@ export async function GET (
   { params: { lang, slug } }: { params: { lang: string, slug: string } }
 ) {
   if (lang === 'zh') lang = 'zh-TW'
-  const jsonPath = path.join(process.cwd(), 'data', 'master', lang, `${slug}.json`)
+  else if (lang === 'ja') lang = 'ja'
+  let jsonPath = path.join(process.cwd(), 'data', 'master', lang, `${slug}.json`)
+  if (lang === 'en') jsonPath = path.join(process.cwd(), 'data/master/en', `${slug}.json`)
   let jsonObj
-  console.log(jsonPath)
+  
   try {
-    const data = await import(`@/data/master/${lang}/${slug}.json`)
-    
+    // method 1
     const fileContents = await fs.readFile(jsonPath, 'utf8')
-    // jsonObj = JSON.parse(fileContents)
-    jsonObj = data.default
+    jsonObj = JSON.parse(fileContents)
+    
+    // method 2
+    // const data = await import(`@/data/master/${lang}/${slug}.json`)
+    // jsonObj = data.default    
   } catch (e) {
     console.error(e)
     jsonObj = {}
