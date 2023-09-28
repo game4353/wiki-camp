c566314c15527f4d7800668602432f5264cc9ecf
game4353 <73753515+game4353@users.noreply.github.com>
2023-09-28 17:55:41 +0800

    remove dupe data in non-JA folders

--- app/[lang]/api/[slug]/route.ts
+++ app/[lang]/api/[slug]/route.ts
@@ -1,6 +1,7 @@
 import path from 'path'
-import { promises as fs } from 'fs'
+import { promises, existsSync } from 'fs'
 import { NextResponse } from 'next/server'
+import { Locale } from '@/i18n-config'
 
 // https://dev.to/martinratinaud/nextjs-on-vercel-missing-files-after-build-when-interpolating-file-names-40bl
 
@@ -8,25 +9,19 @@ export async function GET (
   request: Request,
   { params: { lang, slug } }: { params: { lang: string, slug: string } }
 ) {
-
   let jsonPath = ''
   if (lang === 'ja') jsonPath = path.join(process.cwd(), 'data/master/ja', `${slug}.json`)
   if (lang === 'zh') jsonPath = path.join(process.cwd(), 'data/master/zh-TW', `${slug}.json`)
   if (lang === 'en') jsonPath = path.join(process.cwd(), 'data/master/en', `${slug}.json`)
   if (lang === 'ko') jsonPath = path.join(process.cwd(), 'data/master/ko', `${slug}.json`)
- 
-  let jsonObj
+  if (!existsSync(jsonPath)) jsonPath = path.join(process.cwd(), 'data/master/ja', `${slug}.json`)
+
   try {
-    // method 1
-    const fileContents = await fs.readFile(jsonPath, 'utf8')
-    jsonObj = JSON.parse(fileContents)
-    
-    // method 2
-    // const data = await import(`@/data/master/${lang}/${slug}.json`)
-    // jsonObj = data.default    
+    const fileContents = await promises.readFile(jsonPath, 'utf8')
+    const jsonObj = JSON.parse(fileContents)    
+    return NextResponse.json(jsonObj, { status: 200 })
   } catch (e) {
-    console.error(e)
-    jsonObj = {}
+    console.error(e)  
+    return NextResponse.json({}, { status: 200 })
   }
-  return NextResponse.json(jsonObj, { status: 200 })
 }
